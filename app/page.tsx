bf4979a34e3bef688be213b7ab94ad8e394b68e6
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-04 22:44:27 +0700

    add new logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
+import { Text } from "@/components/Text/Text";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="sign-in-form">
-        <div className="card-header flex flex-col gap-3"> 
+        <div className="card-header flex items-center flex-col gap-3"> 
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
+            src="/ai.svg"
             alt="Next.js Logo"
-            width={180}
-            height={37}
+            width={75}
+            height={75}
             priority
           />
-          <h2>Sign in to your account</h2>
-          <p>Dont have an account?</p>
+          <Text title="Sign in to your account"></Text>
+          <Text text="Dont have an account?"></Text>
         </div>
       </div>
     </main>
