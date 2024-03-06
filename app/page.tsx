b725be5d7b9b14d23c44ca90a89633e8f8a3a9c8
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-06 15:45:10 +0700

    style: add wrapper div for padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import cn from 'classnames'; 
+import cn from 'classnames';
 import Image from "next/image";
 import { Text } from "@/components/Text/Text";
 import { Input } from '@/components/Input';
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="sign-in-form min-w-96 py-5">
-        <div className="card-header flex items-center flex-col gap-1"> 
+        <div className="card-header flex items-center flex-col gap-2">
           <Image
             src="/ai.svg"
             alt="Next.js Logo"
@@ -17,8 +17,10 @@ export default function Home() {
           />
           <Text title="Sign in to your account" className='text-lg font-semibold pt-2'></Text>
           <Text text="Dont have an account?" className='text-gray-500 text-sm'></Text>
-          <Input label="Email" placeholder="you@example.com"/>
-          <Input label="Password" placeholder="Password"/>
+          <div className="flex flex-col w-full gap-3">
+            <Input label="Email" placeholder="you@example.com" />
+            <Input label="Password" placeholder="Password" />
+          </div>
         </div>
       </div>
     </main>
