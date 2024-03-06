7053d80c067679a162e83d1fba500dcfaaef2a02
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-06 15:50:36 +0700

    style: update form structure

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/Input';
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <div className="sign-in-form min-w-96 py-5">
+      <div className="flex flex-col min-w-96 gap-3">
         <div className="card-header flex items-center flex-col gap-2">
           <Image
             src="/ai.svg"
@@ -17,10 +17,10 @@ export default function Home() {
           />
           <Text title="Sign in to your account" className='text-lg font-semibold pt-2'></Text>
           <Text text="Dont have an account?" className='text-gray-500 text-sm'></Text>
-          <div className="flex flex-col w-full gap-3">
-            <Input label="Email" placeholder="you@example.com" />
-            <Input label="Password" placeholder="Password" />
-          </div>
+        </div>
+        <div className="flex flex-col w-full gap-3">
+          <Input label="Email" placeholder="you@example.com" />
+          <Input label="Password" placeholder="Password" />
         </div>
       </div>
     </main>
