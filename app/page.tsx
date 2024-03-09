c0701d2939fc6579ee960975c4087f857501a7b9
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-09 18:50:49 +0700

    style: moved hr to separate component and refactored

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Text } from "@/components/Text/Text";
 import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
+import { HLine } from '@/components/HLine';
 
 export default function Home() {
   return (
@@ -20,21 +21,21 @@ export default function Home() {
           <Text text="Dont have an account?" className='text-gray-500 text-sm'></Text>
         </div>
         <div className="flex flex-col w-full gap-3">
-          <Input label="Email" placeholder="you@example.com" />
-          <Input label="Password" placeholder="Password" />
+          <Input label="Email" placeholder="you@example.com" className="text-sm"/>
+          <Input label="Password" placeholder="Password" className="text-sm"/>
         </div>
         <div className="flex flex-col items-center space-y-4">
           <Button variant="filled" size='s' fullWidth>
             Sign up
           </Button>
           <div className="flex items-center my-4 w-full">
-            <div className="bg-gray-300 h-px flex-grow"></div>
-            <span className="flex-shrink mx-4 text-sm text-gray-600">OR</span>
-            <div className="bg-gray-300 h-px flex-grow"></div>
+            <HLine />
+            <Text className="flex-shrink mx-4 text-sm text-gray-600" text='OR'></Text>
+            <HLine />
           </div>
           <Button variant="clear" size='s' fullWidth>Continue as Seller</Button>
           <div className="text-center text-sm text-gray-500">
-            By clicking continue, you agree to our <a href="#" className="text-blue-600 hover:underline">Terms of Service</a> and <a href="#" className="text-blue-600 hover:underline">Privacy Policy</a>.
+            By clicking continue, you agree to our <a href="#" className="underline">Terms of Service</a> and <a href="#" className="underline">Privacy Policy</a>.
           </div>
         </div>
 
---
+++ components/HLine/HLine.tsx
@@ -0,0 +1,4 @@
+
+
+
+export const HLine = () => <div className="bg-gray-300 h-px flex-grow"></div>
