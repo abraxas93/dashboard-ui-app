7d7a34798234f1c8e11c6504c66153c93e958201
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-11 15:24:43 +0700

    refactoring: replace div elements with flex boxes

--- components/FlexBoxColumn/FlexBoxColumn.tsx
+++ components/FlexBoxColumn/FlexBoxColumn.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { FlexBox, FlexBoxProps } from '../FlexBox';
 import cn from 'classnames';
 
-export const FlexBoxColumt: FC<FlexBoxProps> = (props) => {
+export const FlexBoxColumn: FC<FlexBoxProps> = (props) => {
   const { children, className, ...otherProps } = props;
   return <FlexBox className={cn(className, 'flex-col')} {...otherProps}>{children}</FlexBox>
 }
--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FlexBoxColumn } from "@/components/FlexBoxColumn";
+import { FlexBox } from "@/components/FlexBox";
 
 
 export const Header = () => {
   return (
-    <div className="flex flex-col items-center bg-white shadow w-full">
-      <div className="pt-4 px-4">
-        <div className="flex items-center justify-between px-5 py-2">
-          <div className="flex items-center">
+    <FlexBoxColumn className="items-center bg-white shadow w-full">
+      <div className="pt-4">
+        <FlexBox className="items-center">
+          <FlexBox className="items-center">
             <span className="text-lg font-bold">ChatGPT Mastery</span>
-          </div>
+          </FlexBox>
           <div className="mx-4 flex-1">
             <div className="relative">
               <Image src="search.svg" width={50} height={50} alt="search" className="absolute left-4 top-1/2 h-4 w-4 -translate-y-1/2 transform" />
@@ -27,7 +29,7 @@ export const Header = () => {
               <Image width={50} height={50} src="https://picsum.photos/id/237/50/50" alt="profile" className="mx-3 h-8 w-8 rounded-full" />
             </a>
           </div>
-        </div>
+        </FlexBox>
         <div className="h-18 box-border px-5 flex space-x-8">
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black" aria-current="page">Community</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">classNameroom</Link>
@@ -37,6 +39,6 @@ export const Header = () => {
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">About</Link>
         </div>
       </div>
-    </div>
+    </FlexBoxColumn>
   )
 }
