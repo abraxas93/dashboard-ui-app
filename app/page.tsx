39ae2c93a9a5d89f99d6b4a67c268cd9fd2b7b2a
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-11 15:15:53 +0700

    refactoring: moved header to layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Text } from "@/components/Text/Text";
 import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
 import { HLine } from '@/components/HLine';
-import { Header } from "@/components/Header";
+import { Header } from "@/layout/Header";
 
 export default function Home() {
   return (
