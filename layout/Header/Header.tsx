424c59db59fb6d29f9b51f18bc925c4be6d504fc
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-12 16:54:05 +0700

    refactoring

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -30,14 +30,14 @@ export const Header = () => {
             </a>
           </FlexBox>
         </FlexBox>
-        <div className="h-18 box-border flex space-x-8">
+        <FlexBox className="h-18 box-border space-x-8">
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black" aria-current="page">Community</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">classNameroom</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">Calendar</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">Members</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">Leaderboards</Link>
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black">About</Link>
-        </div>
+        </FlexBox>
       </div>
     </FlexBoxColumn>
   )
