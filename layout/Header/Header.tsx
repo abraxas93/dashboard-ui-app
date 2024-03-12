b110ef6fa94493cd6736c2cfeb374a6eba7857ad
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-12 16:52:45 +0700

    refactoring: replaced div to flexbox

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -18,7 +18,7 @@ export const Header = () => {
               <input type="search" className="w-full rounded-lg border-gray-300 bg-gray-100 px-9 py-2" placeholder="Search" />
             </div>
           </div>
-          <div className="flex items-center">
+          <FlexBox className="flex items-center">
             <a className="">
               <Image width={25} height={25} src="https://icons.getbootstrap.com/assets/icons/chat.svg" alt="chat" className="mx-3 h-6 w-6" />
             </a>
@@ -28,7 +28,7 @@ export const Header = () => {
             <a>
               <Image width={25} height={25} src="https://picsum.photos/id/237/50/50" alt="profile" className="mx-3 h-8 w-8 rounded-full" />
             </a>
-          </div>
+          </FlexBox>
         </FlexBox>
         <div className="h-18 box-border flex space-x-8">
           <Link href="#" className="inline-flex items-center border-b-4 border-transparent py-5 text-base font-medium text-gray-500 hover:border-black hover:text-black" aria-current="page">Community</Link>
