583d9f3d49b844df83f76f907796cf9cb4f758ba
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-15 18:41:24 +0700

    style: add gap between top header element

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -9,14 +9,14 @@ export const Header = () => {
   return (
     <FlexBoxColumn className="items-center bg-white shadow w-full">
       <div className="pt-4">
-        <FlexBox className="items-center">
+        <FlexBox className="items-center gap-3">
           <FlexBox className="items-center">
             <span className="text-lg font-bold">ChatGPT Mastery</span>
           </FlexBox>
           <div className="mx-4 flex-1">
             <Search />
           </div>
-          <FlexBox className="flex items-center gap-2">
+          <FlexBox className="flex items-center gap-3">
             <a className="">
               <Image width={25} height={25} src="https://icons.getbootstrap.com/assets/icons/chat.svg" alt="chat" />
             </a>
