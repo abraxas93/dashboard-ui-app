7c1a533c8702642adc6c86e8154f0c81499cf95e
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-16 19:17:39 +0700

    style: update structure

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,14 @@ export default function Home() {
       <main className="flex">
         <div className="left-col">
           <div className="card">
-
+            <div className="card-header"></div>
+            <div className="card-body"></div>
+            <div className="card-footer"></div>
           </div>
         </div>
         <div className="right-col">
           <div className="widget">
-
+            
           </div>
         </div>
       </main>
