31a71cea941863ebca3ae4d660c999baca1e2b2c
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-20 21:54:33 +0545

    removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Image from "next/image";
-import { Text } from "@/components/Text/Text";
-import { Input } from '@/components/Input';
-import { Button } from '@/components/Button';
-import { HLine } from '@/components/HLine';
 import { Header } from "@/layout/Header";
 
 export default function Home() {
