d31f63f6628cf1ce866217de74069ad194ec1aba
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-21 15:43:05 +0545

    refactoring: made a compound card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,11 +1,51 @@
+import cn from "classnames";
+import { DetailedHTMLProps, FC, HTMLAttributes } from "react"
 
+type DivProps = DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+>;
 
-export const Card = () => {
-  return (
-    <div className="card">
-      <div className="card-header"></div>
-      <div className="card-body"></div>
-      <div className="card-footer"></div>
-    </div>
-  )
+export interface CardHeaderProps extends DivProps {
+  children: React.ReactNode,
+  className?: string,
 }
+const CardHeader: FC<CardHeaderProps> = ({ children }) => {
+  return <div className="card-header">{children}</div>;
+};
+
+export interface CardBodyProps extends DivProps {
+  children: React.ReactNode,
+  className?: string,
+}
+const CardBody: FC<CardBodyProps> = ({ children }) => {
+  return <div className="card-body">{children}</div>;
+};
+
+export interface CardFooterProps extends DivProps {
+  children: React.ReactNode,
+  className?: string,
+}
+
+const CardFooter: FC<CardFooterProps> = ({ children }) => {
+  return <div className="card-footer">{children}</div>;
+};
+
+export interface CardProps extends DivProps {
+  children: React.ReactNode,
+  className?: string,
+}
+
+export const Card: FC<CardProps> & {
+  Header: typeof CardHeader;
+  Body: typeof CardBody;
+  Footer: typeof CardFooter;
+} = ({ children }) => {
+  return <div className="card">{children}</div>;
+};
+
+// Assign subcomponents to the main component
+Card.Header = CardHeader;
+Card.Body = CardBody;
+Card.Footer = CardFooter;
+
