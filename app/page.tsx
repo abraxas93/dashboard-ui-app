f1f2b7ef435c65e68b69892eb77e37bb2cd5a714
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-22 18:03:43 +0545

    refactoring: update card styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
       <Header />
       <main className="flex">
         <div className="left-col w-3/5 p-3">
-          <Card>
-            <Card.Header className="px-3">
+          <Card className="p-3 rounded">
+            <Card.Header>
               <div className="font-bold text-xl mb-2 flex justify-between items-center">
                 <div className="flex items-center">
                   <Image className="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/id/237/50/50" alt="Avatar of Drake Surach" width={50} height={50} />
                     <div className="text-sm">
                       <p className="text-gray-900 leading-none">Drake Surach</p>
-                      <p className="text-gray-600">Jul '23 in General 🌐</p>
+                      <p className="text-gray-600">Jul &apos;23 in General 🌐</p>
                     </div>
                 </div>
                 <div className="flex items-center">
@@ -23,14 +23,14 @@ export default function Home() {
                 </div>
               </div>
             </Card.Header>
-            <Card.Body className="px-3">
+            <Card.Body>
               <p className="text-gray-700 text-base mb-4">
                 Welcome to the ChatGPT Mastery course and community! Watch the video below to learn how to navigate around and get started!
               </p>
             </Card.Body>
-            <Card.Footer className="px-3 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">👍 35</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">💬 18</span>
+            <Card.Footer className="flex items-center gap-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">👍 35</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">💬 18</span>
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Last comment 9d ago</span>
             </Card.Footer>
           </Card>
