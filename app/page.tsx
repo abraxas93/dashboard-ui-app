fec408907dfb7f302ae3578e11c7c42946bbfe36
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-22 17:47:45 +0545

    update: add content to card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Card } from "@/components/Card";
 import { Header } from "@/layout/Header";
 
@@ -8,8 +9,25 @@ export default function Home() {
       <main className="flex">
         <div className="left-col w-3/5">
           <Card>
-            <Card.Header>Card Header</Card.Header>
-            <Card.Body>Card Body</Card.Body>
+            <Card.Header>
+              <div className="font-bold text-xl mb-2 flex justify-between items-center">
+                <div className="flex items-center">
+                  <Image className="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/id/237/50/50" alt="Avatar of Drake Surach" width={50} height={50} />
+                    <div className="text-sm">
+                      <p className="text-gray-900 leading-none">Drake Surach</p>
+                      <p className="text-gray-600">Jul '23 in General 🌐</p>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                  <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2">Getting Started ✔️</span>
+                </div>
+              </div>
+            </Card.Header>
+            <Card.Body>
+              <p className="text-gray-700 text-base mb-4">
+                Welcome to the ChatGPT Mastery course and community! Watch the video below to learn how to navigate around and get started!
+              </p>
+            </Card.Body>
             <Card.Footer className="px-6 pt-4 pb-2">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">👍 35</span>
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">💬 18</span>
