eeb998c343abad9e6009e5a8b32a4196fd323262
Savchuk Alexandr <savchyk.a@gmail.com>
2024-03-23 22:41:01 +0545

    style: applied PostCard to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card } from "@/components/Card";
 import { Header } from "@/layout/Header";
+import { PostCard } from "@/features/PostCard/PostCard";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Header />
       <main className="flex">
         <div className="left-col w-3/5 p-3">
-          
+          <PostCard />
         </div>
         <div className="right-col w-2/5">
           <div className="widget">
