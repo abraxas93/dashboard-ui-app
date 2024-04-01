ffb76e8964fc1a1ecfdea503303b37d9d163585a
Savchuk Alexandr <savchyk.a@gmail.com>
2024-04-01 17:35:43 +0545

    style: update css

--- features/PostCard/PostCard.tsx
+++ features/PostCard/PostCard.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/Card";
+import { FlexBox } from "@/components/FlexBox";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -12,13 +13,13 @@ export const PostCard: FC<PostCardProps> = (props) => {
     <Card className="p-3 rounded">
       <Card.Header>
         <div className="font-bold text-xl mb-2 flex flex-col justify-between gap-2">
-          <div className="flex items-center">
+          <FlexBox className="items-center gap-2">
             <Image className="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/id/237/50/50" alt="Avatar of Drake Surach" width={50} height={50} />
             <div className="text-sm">
               <p className="text-gray-900 leading-none">Drake Surach</p>
               <p className="text-gray-600">Jul &apos;23 in General 🌐</p>
             </div>
-          </div>
+          </FlexBox>
           <div className="flex items-center">
             <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2">Getting Started ✔️</span>
           </div>
